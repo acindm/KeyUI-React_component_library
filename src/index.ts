693a61c6bb6347f909be0fcd74b24ae1dcdd9877
acindms <acindms@gmail.com>
2024-11-29 23:53:05 +0800

    优化
    简化导出结构，并为使用该库的开发者提供一个更加清晰和方便的接口

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import Space from './Space';
 import Button  from './Button';
-import Switch from './Switch';
-import Tabs, { TabsProps } from './Tabs';
+import Switch from './Switch/switch';
+import Tabs, { TabsProps } from './Tabs/tabs';
 import { TabItemType } from './Tabs/types';
 
 export { Space, Button, Switch, Tabs }
